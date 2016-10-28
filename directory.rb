a8580065df3640445c1965b43c4156af9930b2cc
Teako W-Graham <teakowg@gmail.com>
2016-10-28 23:18:43 +0100

    Center align

--- directory.rb
+++ directory.rb
@@ -1,40 +1,33 @@
 # get user to input student names
 def input_students
-  puts "Please enter the name, cohort, birth country, and hobbies of each student"
+  puts "Please enter the name, cohort, birth country, and hobbies of each student".center(10)
   puts "To finish, just hit return twice"
   # create an empty array
   students = []
   # get the first name
-  puts "Enter student name:"
-  name = gets.chomp
-  puts "Enter student cohort"
-	cohort = gets.chomp
-  puts "Enter student birth country:"
-	birth_country = gets.chomp
-  puts "Enter student hobbies"
-  hobbies = gets.chomp
+  name = gets.chomp.center(30)
   # while the name is not empty, repeat this code block
   while !name.empty? do
-    # add the student hash to the array
-    students << {name: name, cohort: cohort, birth: birth_country, hobbies: hobbies}
-    puts "Now we have #{students.count} students"
-    # get another name from the user
-    puts "Enter student name:"
-    name = gets.chomp
     puts "Enter student cohort"
     cohort = gets.chomp
     puts "Enter student birth country:"
     birth_country = gets.chomp
     puts "Enter student hobbies"
     hobbies = gets.chomp
+    # add the student hash to the array
+    students << {name: name, cohort: cohort, birth: birth_country, hobbies: hobbies}
+    puts "Now we have #{students.count} students".center(30)
+    # get another name from the user
+    puts "Enter student name:"
+    name = gets.chomp
   end
   # return the array of students
   students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(30)
+  puts "-------------".center(30)
 end
 
 def print(students)
@@ -47,8 +40,8 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
-students = input_students
 # nothing happens until we call the methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
