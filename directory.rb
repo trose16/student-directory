0566400223de034543ad5ff01e22fec62f6d5196
Teako W-Graham <teakowg@gmail.com>
2016-10-29 23:24:19 +0100

    Saving students to csv

--- directory.rb
+++ directory.rb
@@ -13,6 +13,8 @@ def process (selection)
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit # this will cause the program to terminate
     else
@@ -24,6 +26,7 @@ def print_menu
   # 1. print the menu and ask the user what to do
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit" # 9 because we will be adding more items
   # 2. read the input and save it into a variable
 end
@@ -113,6 +116,18 @@ def print_footer
   else
     puts nil
   end
+end
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 # nothing happens until we call the methods
