e57b44ee74b4f744e85ce842a98ed45b30d5f2ef
Teako W-Graham <teakowg@gmail.com>
2016-10-29 21:44:30 +0100

    Added skeleton menu

--- directory.rb
+++ directory.rb
@@ -1,8 +1,25 @@
 def interactive_menu
-  # 1. print the menu and ask the user what to do
-  # 2. read the input and save it into a variable
-  # 3. do what the user has asked
-  # 4. repeat from step 1
+  loop do
+    # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit" # 9 because we will be adding more items
+    # 2. read the input and save it into a variable
+    selection = gets.chomp
+    # 3. do what the user has asked
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit # this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again please"
+    end
+  end
 end
 
 # get user to input student names
